edc322b9c00cfe68bfc4edb0a68bc46b541dc8df
mrtrader125 <mrtrader125@gmail.com>
2026-03-17 20:45:29 +0530

    Update subscription.ts

--- src/lib/subscription.ts
+++ src/lib/subscription.ts
@@ -1,14 +1,12 @@
 import { supabase } from './supabase'
 
-export async function activatePremium(userId: string){
-
-await supabase
-.from('profiles')
-.update({
-plan:'pro',
-forex_access:true,
-gold_access:true
-})
-.eq('id',userId)
-
+export async function activatePro(userId: string) {
+  await supabase
+    .from('profiles')
+    .update({
+      plan: 'pro',
+      forex_access: true,
+      gold_access: true
+    })
+    .eq('id', userId)
 }
