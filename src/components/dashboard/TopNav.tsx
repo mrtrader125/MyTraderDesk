f8d38bd50433e2b58c2c6b831d74615a360d2e56
mrtrader125 <mrtrader125@gmail.com>
2026-03-27 01:14:45 -0800

    Update TopNav.tsx

--- src/components/dashboard/TopNav.tsx
+++ src/components/dashboard/TopNav.tsx
@@ -66,7 +66,7 @@ function TopNavContent() {
       {/* 🚨 NEW: MOBILE LOGO (Shows only on small screens) */}
       {!isAccountPage && (
         <div className="md:hidden flex items-center mr-3 shrink-0">
-          <Link href="/dashboard" className="relative h-12 w-40 flex items-center block">
+          <Link href="/dashboard" className="relative h-14 w-32 flex items-center block">
             <Image src="/logo.png" alt="My Trader Desk" fill className="object-contain object-left" priority unoptimized />
           </Link>
         </div>
