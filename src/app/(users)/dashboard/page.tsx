cb77d9358d00174873d196dad81fccbea0207d89
mrtrader125 <mrtrader125@gmail.com>
2026-05-09 19:56:38 +0530

    Update page.tsx

--- src/app/(users)/dashboard/page.tsx
+++ src/app/(users)/dashboard/page.tsx
@@ -1,8 +1,8 @@
 // src/app/app/(users)/dashboard/page.tsx
 import DashboardClient from './DashboardClient'
 
-// Notice we removed all Supabase and Server calls. 
-// This page now transitions in 0.00ms.
 export default function DashboardPage() {
+  // ZERO blocking database calls. No Edge runtime.
+  // Next.js serves this instantly.
   return <DashboardClient />
 }
